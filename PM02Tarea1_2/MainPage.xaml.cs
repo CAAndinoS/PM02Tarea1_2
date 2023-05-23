60ff56f40fa19c21b6fa2a2e0e27c4b36d059892
CAAndinoS <124225358+CAAndinoS@users.noreply.github.com>
2023-05-23 16:15:13 -0600

    Agregar archivos de proyecto.

---
+++ PM02Tarea1_2/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PM02Tarea1_2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+        private async void btnenviar_Clicked(object sender, EventArgs e)
+        {
+            var emple = new Models.Empleado
+            {
+                nombres = txtnombre.Text,
+                apellidos = txtapellido.Text,
+                edad = Convert.ToInt32(txtedad.Text),
+                correo = txtcorreo.Text
+            };
+
+            var pagina = new Views.Empleados();
+            pagina.BindingContext = emple;
+            await Navigation.PushAsync(pagina);
+        }
+    }
+}
---
+++ PM02Tarea1_2/Views/Empleados.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PM02Tarea1_2.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Empleados : ContentPage
+    {
+        public Empleados()
+        {
+            InitializeComponent();
+        }
+        public Empleados(String pmessage)
+        {
+            InitializeComponent();
+
+            lbnombres.Text = pmessage;
+            lbapellidos.Text = pmessage;
+            lbedad.Text = pmessage;
+            lbcorreo.Text = pmessage;
+
+
+        }
+    }
+}
